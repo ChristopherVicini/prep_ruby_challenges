28111efe521f5a2444acfec8c2131eb7817a9186
Christopher Vicini <cmvicinipr@gmail.com>
2015-05-10 19:10:30 -0400

    Start of overlap.rb

---
+++ overlap.rb
@@ -0,0 +1,15 @@
+# 6. RECTANGLE OVERLAP
+
+# Write a method (overlap) which takes two (rectangles) defined by the coordinates of their corners, and determines whether they overlap
+#You can assume all coordinates are positive integers.
+
+
+def overlap(rect1, rect2)
+  if #insert argument here
+    return true
+
+  else
+    return false
+  end
+end
+
